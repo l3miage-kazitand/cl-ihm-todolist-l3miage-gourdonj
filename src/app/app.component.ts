6270d011edf620a6ef6e24790088ba6bde4ef219
l3miage-franceto <tom.frances97@yahoo.fr>
2022-02-11 10:33:24 -0100

    TP1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { TodolistService, TodoItem, TodoList } from './todolist.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'l3m-tpX-todolist-angular-y2022';
+
+  readonly todoListObs: Observable<TodoList>;
+
+  constructor(public todoListService: TodolistService){
+    this.todoListObs = todoListService.observable;
+  }
+  ngOnInit(){
+  }
+
+  onSubmit(data: string){
+    console.log(data);
+    this.todoListService.create(data);
+  }
+
+  trackById(i: number, e: TodoItem): number{
+    return e.id;
+  }
+  updateDone(data: boolean,item:TodoItem){
+    //console.log(data)
+    this.todoListService = this.todoListService.update({isDone:data},item)
+    //console.log(this.todoListService)
+  }
+
+  updateLabel(data: string,item:TodoItem){
+    //console.log(data)
+    this.todoListService = this.todoListService.update({label:data},item)
+    //console.log(this.todoListService)
+  }
+  delete(data:TodoItem){
+    this.todoListService.delete(data)
+  }
 }
